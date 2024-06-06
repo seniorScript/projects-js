f763b3b49c66bd7fa1bc536dff2adbf4228c68a9
Linus <reza.linusx@gmail.com>
2024-06-06 17:08:23 +0330

    Display the selected image from user

--- imageColorPicker/app.js
+++ imageColorPicker/app.js
@@ -1,1 +1,18 @@
-console.log('image color picker')
+const imageInput = document.getElementById("image-input");
+const imagePreview = document.getElementById("image-preview");
+
+imageInput.addEventListener("change", displayImage);
+
+function displayImage(event) {
+  const file = event.target.files[0];
+  if (file) {
+    imagePreview.innerHTML = "";
+    const img = document.createElement("img");
+    const source = URL.createObjectURL(file);
+    img.src = source;
+    img.style.display = "block";
+    img.style.width = "50%";
+    img.style.hight = "50%";
+    imagePreview.appendChild(img);
+  }
+}
