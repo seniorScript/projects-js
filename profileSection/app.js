e2220437b814e4ae891e6bbcbcc987cf9493ac9a
Linus <reza.linusx@gmail.com>
2024-06-09 14:53:51 +0330

    Display selected picture on profile

--- profileSection/app.js
+++ profileSection/app.js
@@ -1,6 +1,19 @@
 const imageInput = document.getElementById("choose-image");
+const profileContainer = document.querySelector(".profile-picture");
 const profilePicture = document.querySelector(".profile-picture img");
 
-profilePicture.addEventListener("click", () => {
+profileContainer.addEventListener("click", () => {
   imageInput.click();
 });
+
+imageInput.addEventListener("change", (e) => displayImage(e));
+
+function displayImage(e) {
+  const file = e.target.files[0];
+  if (file) {
+    const img = document.createElement("img");
+    img.src = URL.createObjectURL(file);
+    profileContainer.innerHTML = "";
+    profileContainer.appendChild(img);
+  }
+}
