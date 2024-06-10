d1bc5a54b9a293090d5142148b511391cdd6c320
Linus <reza.linusx@gmail.com>
2024-06-10 17:26:55 +0330

    Get stopwatch elements from UI

--- stopwatch/app.js
+++ stopwatch/app.js
@@ -1,1 +1,8 @@
-console.log("stopwatch");
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const seconds = document.getElementById("seconds");
+const miliseconds = document.getElementById("miliseconds");
+
+const start = document.getElementById("start");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
