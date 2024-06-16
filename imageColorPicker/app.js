26dbf607fd2ea289af71da17a48e117aacb67df5
Linus <reza.linusx@gmail.com>
2024-06-16 09:32:21 +0330

    Update rbg to hex function

--- imageColorPicker/app.js
+++ imageColorPicker/app.js
@@ -102,14 +102,14 @@ async function copyText(text) {
   }
 }
 
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+const rgbToHex = (r, g, b) =>
+  "#" +
+  [r, g, b]
+    .map((x) => {
+      const hex = x.toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    })
+    .join("");
 
 // event listeners
 colorSwitcher.addEventListener("click", () => {
