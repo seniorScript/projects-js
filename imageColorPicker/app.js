7c4ccaa15b194588bb623daed1490d20392a2b13
Linus <reza.linusx@gmail.com>
2024-06-16 09:29:41 +0330

    Create functions to change RGB to Hex

--- imageColorPicker/app.js
+++ imageColorPicker/app.js
@@ -102,6 +102,15 @@ async function copyText(text) {
   }
 }
 
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 // event listeners
 colorSwitcher.addEventListener("click", () => {
   if (mode === "color") return;
