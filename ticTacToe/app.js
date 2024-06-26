4d629af944a72faeec343def8e4fe4a8a56cc384
Linus <reza.linusx@gmail.com>
2024-06-26 18:40:22 +0330

    Add drag and drop listener for game cells

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -2,6 +2,11 @@ const cells = document.querySelectorAll("[data-cell]");
 const draggables = document.querySelectorAll(".draggable");
 let currentPlayer = "X";
 
+cells.forEach((cell) => {
+  cell.addEventListener("dragover", dragOver);
+  cell.addEventListener("drop", drop);
+});
+
 function dragStart(e) {
   e.dataTransfer.setData("text/plain", e.target.id);
 }
