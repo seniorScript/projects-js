1d86b27009a5b3a57aa8af0432630e65acfd5fe2
Linus <reza.linusx@gmail.com>
2024-06-26 18:39:59 +0330

    Add dragstart and dragover functions

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -1,3 +1,11 @@
 const cells = document.querySelectorAll("[data-cell]");
 const draggables = document.querySelectorAll(".draggable");
 let currentPlayer = "X";
+
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.id);
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
