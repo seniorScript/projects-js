831df587e75538edd4003cf380c9d5f3206fcd12
Linus <reza.linusx@gmail.com>
2024-06-26 18:41:05 +0330

    Create functions to determine the winner

--- ticTacToe/app.js
+++ ticTacToe/app.js
@@ -33,3 +33,22 @@ function drop(e) {
     }
   }
 }
+
+function checkWin(player) {
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  return winCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cells[index].textContent === player;
+    });
+  });
+}
