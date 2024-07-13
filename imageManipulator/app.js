2c2047af7f278093e5859a12fa290e3c27c01cb3
Linus <reza.linusx@gmail.com>
2024-07-13 20:40:47 +0330

    Dynamically choose color display text based on contrast

--- imageManipulator/app.js
+++ imageManipulator/app.js
@@ -158,8 +158,20 @@ function handleColorPicker(e) {
   const data = context.getImageData(pos.x, pos.y, 1, 1).data;
   const [R, G, B] = data;
   const hex = RgbToHex(R, G, B);
+
   pickedColor.style.background = hex;
   pickedColor.innerHTML = hex;
+
+  const textColor = getContrastYIQ(hex);
+  pickedColor.style.color = textColor;
+}
+
+function getContrastYIQ(hexcolor) {
+  const r = parseInt(hexcolor.substr(1, 2), 16);
+  const g = parseInt(hexcolor.substr(3, 2), 16);
+  const b = parseInt(hexcolor.substr(5, 2), 16);
+  const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  return yiq >= 128 ? "black" : "white";
 }
 
 function handleCrop(e) {
